b10389f933d6ecca5be292297f9a41d6c5bfc07f
charan-web <cc710788@gmail.com>
2020-08-30 05:47:47 +0530

    uploading files

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const bird = document.querySelector(".bird")
 isGameOver = false;
 const button = document.querySelector(".btn")
 let h1 = document.querySelector(".h1")
+const scoreId = document.getElementById("score")
+let score = document.querySelector(".ptag").innerHTML
+
+let value;
+
 
 
 let birdBottom = 150
@@ -26,7 +31,7 @@ function control(e) {
   }
 }
 
-
+//function for jump when key is pressed
 function jump() {
   if (birdBottom < 500) birdBottom += 50
   bird.style.bottom = birdBottom + 'px'
@@ -34,30 +39,36 @@ function jump() {
 document.addEventListener("keyup", control)
 
 
-//obstacles
+//create obstacles
 function createObstacles() {
   let obstacleLeft = 500
 
   let obstacleBottom = 0
-
+//top obstacle = top pipe
+//obstacle = bottom pipe
   const obstacle = document.createElement("div")
   const topObstacle = document.createElement("div")
   if (!isGameOver) {
     obstacle.classList.add("obstacle")
     topObstacle.classList.add("topObstacle")
   }
-
+//random height for top and bottom pipes 
   obstacle.style.height = Math.random() * 300 + "px"
   topObstacle.style.height = Math.random() * 200 + "px"
 
+//append the pipes to game container
   gameContainer.appendChild(obstacle)
   gameContainer.appendChild(topObstacle)
+
+
+//positioning the pipes in game container
   obstacle.style.left = obstacleLeft + "px"
   topObstacle.style.left = obstacleLeft + 'px'
 
   obstacle.style.bottom = obstacleBottom + 'px'
 
 
+  //moving the pipes towards the bird
   function moveLeft() {
     if (!isGameOver) {
 
@@ -69,20 +80,38 @@ function createObstacles() {
 
 
 
-
+   //removing the pipes after it leaves game container
     if (obstacleLeft === -50 && topObstacle === -50) {
       clearInterval(timerId)
       gameContainer.removeChild(obstacle)
       gameContainer.removeChild(topObstacle)
     }
+
+    //finding the collapse means when the bird hits pipes 
+    //numbers indicate the position of objects
+    //where pipes position and bird are at same that means COLLIDING
     if (obstacleLeft > 150 && obstacleLeft < 260 && birdLeft === 200 &&
       (birdBottom < obstacle.clientHeight || birdBottom > (580 - topObstacle.clientHeight)) ||
       birdBottom === 0) {
       gameOver()
+     
       clearInterval(timerId)
 
     }
+    //score
+    else if(obstacleLeft === 30){
+      value = parseInt(score)
+       value++
+     score = value
+    document.querySelector('p').innerHTML = score
+   
+      
+    
+    
+      //console.log(score)
+    }
   }
+  //if bird hit anyone of the pipe the we need stop the pipes moving function
   let timerId = setInterval(moveLeft, 20)
 
   if (!isGameOver) setTimeout(createObstacles, 3000)
@@ -95,10 +124,33 @@ function createObstacles() {
 
 }
 createObstacles()
-
+//game over function after collapse
 function gameOver() {
   clearInterval(gamerTImerId)
   isGameOver = true;
   document.removeEventListener("keyup", control)
   h1.innerHTML = " Game over"
+  document.getElementById("highScore").innerHTML = "#Your High score is " +"  "  +  score
+  stopClouds()
+  
 }
+
+//stops the clouds animation
+function stopClouds(){
+ let span = document.getElementsByTagName("span")
+ let array = [span]
+ array.forEach(element => {
+ for(item of element){
+   item.style.webkitAnimationPlayState = "paused";
+ }
+ });
+}
+
+function restart(){
+  document.location.href=""
+}
+
+
+
+ 
+
